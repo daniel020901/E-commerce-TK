6a4a7a71d16d31ec551082e26628790302e4a96d
daniel020901 <muhammaddanielilyasa@gmail.com>
2024-04-26 10:49:15 +0700

    kedua

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card shadow cart-product">
+    <img
+      :src="'assets/images/' + product.gambar"
+      class="card-img-fluid p-2"
+      alt="..."
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ product.nama }}</h5>
+      <p class="card-text">Rp.{{ product.harga }}</p>
+      <button class="btn btn-success">
+        <b-icon-cart></b-icon-cart
+        ><a
+          href="https://app.sandbox.midtrans.com/payment-links/1714102023688"
+          class="text-white no-underline"
+          >Pesan</a
+        >
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardProduct",
+  props: ["product"],
+};
+</script>
+
+<style>
+</style>
